895f10d5af949951ec417ce9c7153fa82c5d1dcd
SkyLightQP <combbm@naver.com>
2018-11-09 20:56:09 +0900

    Ping 명령어 지연시간 구하기 구현

--- src/main/kotlin/kr/skylightqp/sweetbot/commands/PingCommand.kt
+++ src/main/kotlin/kr/skylightqp/sweetbot/commands/PingCommand.kt
@@ -5,18 +5,25 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import java.awt.Color
 
 class PingCommand : ICommand {
-
     override var name: String = "핑"
     override var alias: Array<String> = arrayOf("ping")
 
     override fun execute(args: Array<String>, event: MessageReceivedEvent) {
+        val startTime = System.currentTimeMillis() / 1000
         val channel = event.channel
 
         val eb = EmbedBuilder()
                 .setColor(Color(46, 204, 113))
                 .setTitle("Ping? Pong!")
-                .setDescription("Pong!")
+                .setDescription("Pong...?")
 
-        channel.sendMessage(eb.build()).queue()
+        channel.sendMessage(eb.build()).queue {
+            val endTime = System.currentTimeMillis() / 1000
+            it.editMessage(EmbedBuilder()
+                    .setColor(Color(46, 204, 113))
+                    .setTitle("Ping? Pong!")
+                    .setDescription("Pong! ${endTime - startTime}ms")
+                    .build()).queue()
+        }
     }
 }
